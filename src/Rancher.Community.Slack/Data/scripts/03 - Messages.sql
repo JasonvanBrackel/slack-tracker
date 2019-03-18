0965707268f7e5d80a739ddddf00f418fa7f659a
Jason L. van Brackel <jason@vanbrackel.net>
2019-03-18 11:12:25 -0400

    Add ci script, Modify messages to accept null values

--- src/Rancher.Community.Slack/Data/scripts/03 - Messages.sql
+++ src/Rancher.Community.Slack/Data/scripts/03 - Messages.sql
@@ -5,7 +5,7 @@ CREATE TABLE [dbo].[Messages] (
   EventId nvarchar(255) NOT NULL,
   Channel nvarchar(255) NOT NULL,
   [User] nvarchar(255) NOT NULL,
-  Text ntext NOT NULL,
+  Text ntext,
   Timestamp datetime2 NOT NULL,
   CONSTRAINT PK_Messages PRIMARY KEY (EventId),
   -- CONSTRAINT FK_Channels_Messages FOREIGN KEY (Channel)
